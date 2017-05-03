cd78ab8b61595209f2951a4c543a81b41e0e0640
Jason Wilson <jwilsonjx@outlook.com>
2017-05-03 09:53:07 -0400

    Singleton

---
+++ DesignPatterns/Patterns/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+
+        static Singleton() { }
+
+        public Singleton()
+        {
+            // Stuff that must only happen once.
+            Console.WriteLine("Singleton constructor");
+        }
+
+        public static Singleton Instance { get { return instance; } }
+
+        public void DoSomething()
+        {
+
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace DesignPatterns
 {
+    [TestFixture]
     class Program
     {
-        static void Main(string[] args)
+           static void Main(string[] args)
         {
+            Program p = new Program();
+            p.UseSingleton();            
+        }
+
+        [Test]
+        public void UseSingleton()
+        {
+            Console.WriteLine("Start of Singleton Test");
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+            Assert.AreSame(s1, s2);
         }
     }
 }
